11b25e84ac222ba6ba3e2c965336d64619c2bced
hum-bat <151434652+hum-bat@users.noreply.github.com>
2023-11-23 20:42:29 +0100

    Add files via upload

---
+++ js/slovo.js
@@ -0,0 +1,13 @@
+let allText = `
+
+<li><span>Martin | 250Kč    | ******0143/0800 | 21.11.2023</span></li>
+<li><span>Otakar | 1 000Kč  | ******7010/3030 | 21.11.2023</span></li>
+<li><span>Rudolf | 500Kč    | ******0560/2010 | 22.11.2023</span></li>
+<li><span>Jiří   | 2 000Kč  | ******1001/5500 | 23.11.2023</span></li>
+
+`;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.querySelector("#myUL").innerHTML = allText;
+    allText = '';
+});
---
+++ js/txt.js
@@ -0,0 +1,20 @@
+// [20.11.2023 -]
+
+const dataX = `
+
+22.11.2023 -
+
+`;
+
+// [3 750]
+
+const sumaX = `
+
+3 750
+
+`;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+        document.querySelector(".sos-mini").innerHTML = dataX;
+        document.querySelector(".sos-sum").innerHTML = sumaX;
+});
