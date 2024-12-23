d0fb7ffbd7abd5443718617bad79b754a1d7518b
hum-bat <151434652+hum-bat@users.noreply.github.com>
2024-12-24 00:56:31 +0300

    Delete all 2024
    Delete all 2024

--- seznam/js/save-restore-scroll.js
+++
@@ -1,3 +0,0 @@
-let cords = ['scrollX', 'scrollY'];
-window.addEventListener('unload', e => cords.forEach(cord => localStorage[cord] = window[cord]));
-window.scroll(...cords.map(cord => localStorage[cord]));
